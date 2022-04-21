15020ef07cbf9c420eb386b157b9bacd0871a7d8
EddYam <edder_yamir@hotmail.com>
2022-04-20 19:27:36 -0500

    Agregando prueba de unidad para la clase spiderman

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,13 @@
-describe("Test Suite Dummy Description", () => {
-    test("Case 1 Dummy", () => {
-        const resultOfSomething = 2 + 1
-        expect(resultOfSomething).toBe(18);
+describe("Unit Tests for Spiderman class", () => {
+    test("1) Create an spiderman object", () => {
+        //Instanciando objeto spiderman con la siguiente informacion
+        const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
+
+        //Validar que codigo funcione
+        expect(andrewGarfield.name).toBe("Spiderman Sony")
+        expect(andrewGarfield.age).toBe(31)
+        expect(andrewGarfield.actor).toBe("Andrew Garfield")
+        expect(andrewGarfield.movies).toBe(31)
+        expect(andrewGarfield.studio).toBe("Sony")
     });
 })
