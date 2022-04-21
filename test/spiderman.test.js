1f9b3b7848a3ecbea57ba03bd0ba77ada66fc40e
EddYam <edder_yamir@hotmail.com>
2022-04-20 19:47:11 -0500

    Agregando prueba para el metodo getInfo()

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -9,7 +9,11 @@ describe("Unit Tests for Spiderman class", () => {
         expect(andrewGarfield.name).toBe("Spiderman Sony")
         expect(andrewGarfield.age).toBe(31)
         expect(andrewGarfield.actor).toBe("Andrew Garfield")
-        expect(andrewGarfield.movies).toBe(31)
+        expect(andrewGarfield.movies).toBe(2)
         expect(andrewGarfield.studio).toBe("Sony")
     });
+    test("2) Use the method getInfo()", () => {
+        const tomHolland = new Spiderman("Spierman Marvel", 25, "Tom Holland", 5, "Marvel")
+        expect(tomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel studio")
+    })
 })
